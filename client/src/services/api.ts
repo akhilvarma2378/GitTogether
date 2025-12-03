a75f12be2e3dd0cf569c852227a368a7d04081bc
akhilvarma2378 <akhilvarma2378@gmail.com>
2025-12-04 03:30:47 +0530

    backend baseurl change

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+
 const api = axios.create({
-  baseURL: 'http://192.168.31.44:3000/api', // Make sure this matches your backend port
+  baseURL: BASE_URL,// Make sure this matches your backend port
 });
 
 // Add Token to every request if it exists
