448a0866afbf1fe183ec30f3d6dacebb81d08518
akhilvarma2378 <akhilvarma2378@gmail.com>
2025-12-10 03:51:16 +0530

    auth middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -25,7 +25,7 @@ export const authenticate = (req: AuthRequest, res: Response, next: NextFunction
     
     const decoded = verifyToken(token); 
     
-    req.user = { userId: decoded.userId};
+    req.user = decoded; 
     next();
   } catch (error) {
     res.status(401).json({ message: "Invalid or expired token" });
